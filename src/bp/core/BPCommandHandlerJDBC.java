c7141542c8d411f727bb252e96da7975cf90c6b7
myhfit <myhfit@126.com>
2025-09-08 22:41:08 +0800

    * monthly upadte add list db command

--- src/bp/core/BPCommandHandlerJDBC.java
+++ src/bp/core/BPCommandHandlerJDBC.java
@@ -1,5 +1,6 @@
 package bp.core;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
 
@@ -13,6 +14,7 @@ import bp.jdbc.BPJDBCContextBase;
 import bp.jdbc.SQLCMDTYPE;
 import bp.jdbc.BPJDBCContextBase.BPJDBCContextEnv;
 import bp.jdbc.BPJDBCContextBase.JDBCThread;
+import bp.project.BPResourceProject;
 import bp.project.BPResourceProjectJDBC;
 import bp.res.BPResource;
 import bp.res.BPResourceJDBCLink;
@@ -23,10 +25,11 @@ import bp.util.Std;
 public class BPCommandHandlerJDBC extends BPCommandHandlerBase implements BPCommandHandler
 {
 	public final static String CN_DB_USE = "db_use";
+	public final static String CN_DB_LIST = "db_list";
 
 	public BPCommandHandlerJDBC()
 	{
-		m_cmdnames = ObjUtil.makeList(CN_DB_USE);
+		m_cmdnames = ObjUtil.makeList(CN_DB_USE, CN_DB_LIST);
 	}
 
 	public BPCommandResult call(BPCommand cmd)
@@ -38,6 +41,10 @@ public class BPCommandHandlerJDBC extends BPCommandHandlerBase implements BPComm
 			case CN_DB_USE:
 			{
 				return useDB(getPSStringArr(cmd.ps));
+			}
+			case CN_DB_LIST:
+			{
+				return listDB(getPSStringArr(cmd.ps));
 			}
 		}
 		return null;
@@ -70,6 +77,33 @@ public class BPCommandHandlerJDBC extends BPCommandHandlerBase implements BPComm
 		return BPCommandResult.OK(null);
 	}
 
+	protected BPCommandResult listDB(String[] ps)
+	{
+		List<BPResourceProjectJDBC> prjs = new ArrayList<BPResourceProjectJDBC>();
+		{
+			BPResourceProject[] prjarr = BPCore.getProjectsContext().listProject();
+			for (BPResourceProject prj : prjarr)
+			{
+				if (prj.getProjectTypeName() == BPResourceProjectJDBC.PRJTYPE_JDBC)
+				{
+					prjs.add((BPResourceProjectJDBC) prj);
+				}
+			}
+		}
+		StringBuilder sb = new StringBuilder();
+		for (BPResourceProjectJDBC prj : prjs)
+		{
+			List<BPResource> ress = prj.getProjectFunctionItems();
+			for (BPResource res : ress)
+			{
+				if (sb.length() > 0)
+					sb.append("\n");
+				sb.append("[" + prj.getName() + "]" + res.getName());
+			}
+		}
+		return BPCommandResult.OK(sb.toString());
+	}
+
 	public String getName()
 	{
 		return "JDBC";
@@ -83,6 +117,8 @@ public class BPCommandHandlerJDBC extends BPCommandHandlerBase implements BPComm
 
 		private final static String CN_CONNECT = "connect";
 		private final static String CN_DISCONNECT = "disconnect";
+		private final static String CN_COMMIT = "commit";
+		private final static String CN_ROLLBACK = "rollback";
 
 		public BPDBCLIContext(BPResourceJDBCLink jdbclink)
 		{
@@ -110,7 +146,7 @@ public class BPCommandHandlerJDBC extends BPCommandHandlerBase implements BPComm
 				}
 				case CN_HELP:
 				{
-					rc = BPCommandResult.OK(String.join("\n", CN_CONNECT, CN_DISCONNECT, CN_EXIT, "or direct input SQL to run"));
+					rc = BPCommandResult.OK(String.join("\n", CN_CONNECT, CN_DISCONNECT, CN_COMMIT, CN_ROLLBACK, CN_EXIT, "or direct input SQL to run"));
 					break;
 				}
 				case CN_CONNECT:
@@ -121,6 +157,16 @@ public class BPCommandHandlerJDBC extends BPCommandHandlerBase implements BPComm
 				case CN_DISCONNECT:
 				{
 					rc = BPCommandResult.OK(LogicUtil.catchGet(() -> m_context.disconnect().toCompletableFuture().get(), false));
+					break;
+				}
+				case CN_COMMIT:
+				{
+					rc = BPCommandResult.OK(LogicUtil.catchGet(() -> m_context.commit().toCompletableFuture().get(), false));
+					break;
+				}
+				case CN_ROLLBACK:
+				{
+					rc = BPCommandResult.OK(LogicUtil.catchGet(() -> m_context.rollback().toCompletableFuture().get(), false));
 					break;
 				}
 				default:
